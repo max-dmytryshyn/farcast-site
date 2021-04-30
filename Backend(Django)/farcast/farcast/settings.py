75c8f757d4f0a7a99d42a952a341f1a09df44691
Danyaruko <danilpatana1905@gmail.com>
2021-04-30 20:10:50 +0300

    Fixed django_countries app

--- Backend(Django)/farcast/farcast/settings.py
+++ Backend(Django)/farcast/farcast/settings.py
@@ -35,7 +35,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django-countries',
+    'django_countries',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
