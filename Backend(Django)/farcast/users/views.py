44b6a900ff81266de084fc3bdac2b6c057f7b9e1
max-dmytryshyn <maksumus22@gmail.com>
2021-05-01 22:59:20 +0300

    Add view for FarCastUser with access only with jwt token

--- Backend(Django)/farcast/users/views.py
+++ Backend(Django)/farcast/users/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets
+
+from .serializers import FarCastUserSerializer
+from .models import FarCastUser
+from rest_framework.permissions import IsAuthenticated
+
+
+class FarCastUserSet(viewsets.ModelViewSet):
+    permission_classes = (IsAuthenticated,)
+
+    queryset = FarCastUser.objects.all()
+    serializer_class = FarCastUserSerializer
