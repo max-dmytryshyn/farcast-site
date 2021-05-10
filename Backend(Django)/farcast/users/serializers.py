4153ea403725a846a943b7de38127725368b8cf5
max-dmytryshyn <maksumus22@gmail.com>
2021-05-10 19:11:30 +0300

    Remove unnecessary fields in FarCastUserSeializer

--- Backend(Django)/farcast/users/serializers.py
+++ Backend(Django)/farcast/users/serializers.py
@@ -6,7 +6,5 @@ from .models import FarCastUser
 class FarCastUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = FarCastUser
-        fields = (
-                    'username', 'email', 'first_name', 'last_name', 'display_name', 'date_of_birth', 'address1',
-                    'address2', 'zip_code', 'city', 'mobile_phone', 'additional_information'
-                 )
+        fields = ('username', 'email', 'first_name', 'last_name',  'mobile_phone', 'password')
+        
