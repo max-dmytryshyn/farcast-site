7182dfe96daba50454f22af3766a233c58c252f4
max-dmytryshyn <maksumus22@gmail.com>
2021-05-23 21:49:05 +0300

    Create ProductForPrediction and SalePrediction models

--- Backend(Django)/farcast/prediction_tables/models.py
+++ Backend(Django)/farcast/prediction_tables/models.py
@@ -1,2 +1,19 @@
 from django.db import models
+from django.core.validators import MaxValueValidator
 
+
+class ProductForPrediction(models.Model):
+    name = models.CharField(max_length = 32)
+    price = models.FloatField()
+
+    class Meta:
+        db_table = "prediction_tables_product_for_prediction"
+
+
+class SalePrediction(models.Model):
+    product = models.ForeignKey(ProductForPrediction, on_delete = models.CASCADE)
+    hour = models.PositiveSmallIntegerField(MaxValueValidator(23))
+    amount = models.IntegerField()
+
+    class Meta:
+        db_table = "prediction_tables_sale_prediction"
