56e9956ed9d2ea9fb2d06a4ed06cdfae40618634
Mikola-K <hotoneshot.ua@gmail.com>
2021-05-25 17:04:30 +0300

    Connect to registration page

--- Frontend(React)/src/App.js
+++ Frontend(React)/src/App.js
@@ -3,12 +3,14 @@ import React from "react"
 import {Route, Link} from "react-router-dom"
 import Homepage from "./components/homepage/Homepage"
 import Login from "./components/login-page/Login"
+import Registration from "./components/registration-page/Registration"
 
 function App() {
     return (
         <div>
             <Route path="/home" component={Homepage} />
             <Route path="/login" component={Login} />
+            <Route path="/registration" component={Registration} />
         </div>
     )
 }
