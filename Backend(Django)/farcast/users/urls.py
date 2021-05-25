c6a0bdda6b07c318b7de646880bc9b98ff5a5555
max-dmytryshyn <maksumus22@gmail.com>
2021-05-25 21:13:09 +0300

    Change path for obtaining pair of jwt tokens(log in)

--- Backend(Django)/farcast/users/urls.py
+++ Backend(Django)/farcast/users/urls.py
@@ -8,6 +8,6 @@ router.register(r'users', views.FarCastUserViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('login/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
 ]
