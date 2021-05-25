a710615a42f17456623627138931c713b950f83e
max-dmytryshyn <maksumus22@gmail.com>
2021-05-25 21:10:33 +0300

    Commend dependency rest_framework.authtoken, which caused problems when someone tries to delete user from database

--- Backend(Django)/farcast/farcast/settings.py
+++ Backend(Django)/farcast/farcast/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'users.apps.UsersConfig',
     'rest_framework',
-    'rest_framework.authtoken',
+    # 'rest_framework.authtoken',
     'corsheaders'
 ]
 
