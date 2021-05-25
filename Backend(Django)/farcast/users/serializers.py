485a30f3246991f8c5420c09fe12b26f06233361
max-dmytryshyn <maksumus22@gmail.com>
2021-05-25 21:09:16 +0300

    Remove mobile phone field, add id field, rewrite update method to make it work for partial updates too

--- Backend(Django)/farcast/users/serializers.py
+++ Backend(Django)/farcast/users/serializers.py
@@ -6,7 +6,7 @@ from .models import FarCastUser
 class FarCastUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = FarCastUser
-        fields = ('username', 'email', 'first_name', 'last_name',  'mobile_phone', 'password')
+        fields = ('username', 'email', 'first_name', 'last_name', 'password', 'id')
 
     def create(self, validated_data):
         user = FarCastUser.objects.create(
@@ -20,11 +20,13 @@ class FarCastUserSerializer(serializers.ModelSerializer):
         return user
 
     def update(self, instance, validated_data):
-        instance.first_name = validated_data['first_name']
-        instance.last_name = validated_data['last_name']
-        instance.email = validated_data['email']
-        instance.mobile_phone = validated_data['mobile_phone']
-        instance.set_password(validated_data['password'])
+        instance.first_name = validated_data.get('first_name', instance.first_name)
+        instance.last_name = validated_data.get('last_name', instance.last_name)
+        instance.email = validated_data.get('email', instance.email)
+        instance.set_password(validated_data.get('password', instance.password))
         instance.save()
         return instance
+
+
+
 
