71a4bbadc72ce13329ba56bd75b6041b8ecb11fe
Kleefeelf <wladfeel@gmail.com>
2021-05-25 11:58:53 +0300

    Fixed footer contacts section

--- Frontend(React)/src/components/homepage/Footer.js
+++ Frontend(React)/src/components/homepage/Footer.js
@@ -1,16 +1,14 @@
 import React from "react"
 
-import FirstColumn from "./footer-parts/Contacts"
+import ContactsColumn from "./footer-parts/Contacts"
 
 
 function Footer() {
     return (
         <div id="contacts">
             <div>
-                <a id="contacts_head">Контакти</a><br/>
-                <a id="contacts_quote">Перший в Україні хлібопекарний прогнозер</a>
             </div>
-            <FirstColumn /> 
+            <ContactsColumn /> 
         </div>
     )
 }
--- Frontend(React)/src/components/homepage/footer-parts/Contacts.js
+++ Frontend(React)/src/components/homepage/footer-parts/Contacts.js
@@ -1,14 +1,14 @@
 import React from "react"
 
-function FirstColumn() {
+function ContactsColumn() {
     return (
-        <div id="first_column">
+        <div id="contacts_column">
             <ul>
-                <div className="column_head">Contacts</div>
-                <li>E-mail: greenflashteam@gmail.com</li>
+                <div className="column_head">Контакти</div>
+                <li><span className="mail">E-mail:</span> greenflashteam@gmail.com</li>
             </ul>
         </div>
     )
 }
 
-export default FirstColumn
+export default ContactsColumn
