38ac9069f59d3e7bd284d5f9a354f6aea641e34d
max-dmytryshyn <maksumus22@gmail.com>
2021-05-26 13:13:07 +0300

    Create view set for SalePrediction model

--- Backend(Django)/farcast/prediction_tables/views.py
+++ Backend(Django)/farcast/prediction_tables/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 
-from .models import ProductForPrediction
-from .serializers import ProductForPredictionSerializer
+from .models import ProductForPrediction, SalePrediction
+from .serializers import ProductForPredictionSerializer, SalePredictionSerializer
 
 
 class ProductForPredictionViewSet(viewsets.ModelViewSet):
 
     queryset = ProductForPrediction.objects.all()
     serializer_class = ProductForPredictionSerializer
+
+
+class SalePredictionViewSet(viewsets.ModelViewSet):
+
+    queryset = SalePrediction.objects.all()
+    serializer_class = SalePredictionSerializer
+
