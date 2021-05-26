13829c5984da479d42d1c0586d3b5618d2e12ec2
max-dmytryshyn <maksumus22@gmail.com>
2021-05-26 15:54:30 +0300

    Change path to prediction tables app

--- Backend(Django)/farcast/farcast/urls.py
+++ Backend(Django)/farcast/farcast/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('prediction/', include('prediction_tables.urls')),
+    path('prediction_tables/', include('prediction_tables.urls')),
 ]
