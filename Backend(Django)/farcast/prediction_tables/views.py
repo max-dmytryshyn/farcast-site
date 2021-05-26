a66ad08921ec0d9d94461c32ca1411e95781c06e
max-dmytryshyn <maksumus22@gmail.com>
2021-05-26 16:47:27 +0300

    Add not existing date exeption handling, customize no predictions message

--- Backend(Django)/farcast/prediction_tables/views.py
+++ Backend(Django)/farcast/prediction_tables/views.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ValidationError
 from django.shortcuts import render
 from rest_framework import viewsets, status
 from rest_framework.decorators import action
@@ -20,9 +21,12 @@ class SalePredictionViewSet(viewsets.ModelViewSet):
 
     @action(methods=['get'], detail=False, url_path='date/(?P<date>\d{4}-\d{2}-\d{2})')
     def get_sale_predictions_by_date(self, request, date):
-        predictions = self.queryset.filter(day = date)
+        try:
+            predictions = self.queryset.filter(day = date)
+        except ValidationError:
+            return Response("Invalid date", status=status.HTTP_400_BAD_REQUEST)
         if predictions:
             data = SalePredictionSerializer(predictions, many=True).data
             return Response(data, status=status.HTTP_200_OK)
         else:
-            return Response("There are no predictions for this day", status=status.HTTP_404_NOT_FOUND)
+            return Response(f"There are no predictions for {date}", status=status.HTTP_404_NOT_FOUND)
