cbe19332fc0d45af1ebd603537a640be419491fc
max-dmytryshyn <maksumus22@gmail.com>
2021-05-26 15:19:29 +0300

    Add get all predictions by date method

--- Backend(Django)/farcast/prediction_tables/views.py
+++ Backend(Django)/farcast/prediction_tables/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from .models import ProductForPrediction, SalePrediction
 from .serializers import ProductForPredictionSerializer, SalePredictionSerializer
@@ -16,3 +18,11 @@ class SalePredictionViewSet(viewsets.ModelViewSet):
     queryset = SalePrediction.objects.all()
     serializer_class = SalePredictionSerializer
 
+    @action(methods=['get'], detail=False, url_path='date/(?P<date>\d{4}-\d{2}-\d{2})')
+    def get_sale_predictions_by_date(self, request, date):
+        predictions = self.queryset.filter(day = date)
+        if predictions:
+            data = SalePredictionSerializer(predictions, many=True).data
+            return Response(data, status=status.HTTP_200_OK)
+        else:
+            return Response("There are no predictions for this day", status=status.HTTP_404_NOT_FOUND)
