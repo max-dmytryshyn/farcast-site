e0550582994b8e53e130d83fe38689f95b69b6f1
max-dmytryshyn <maksumus22@gmail.com>
2021-05-26 13:13:55 +0300

    Connect SalePrediction view set to path system

--- Backend(Django)/farcast/prediction_tables/urls.py
+++ Backend(Django)/farcast/prediction_tables/urls.py
@@ -5,6 +5,7 @@ from . import views
 
 router = routers.DefaultRouter()
 router.register(r'products', views.ProductForPredictionViewSet)
+router.register(r'predictions', views.SalePredictionViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
