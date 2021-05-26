601facf7a64960eca8ad64653fa13e410d172b2e
max-dmytryshyn <maksumus22@gmail.com>
2021-05-26 15:19:04 +0300

    Add min value validation for amount field

--- Backend(Django)/farcast/prediction_tables/models.py
+++ Backend(Django)/farcast/prediction_tables/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.core.validators import MaxValueValidator
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.utils import timezone
 
 
@@ -15,7 +15,7 @@ class SalePrediction(models.Model):
     day = models.DateField()
     hour = models.PositiveSmallIntegerField(MaxValueValidator(23))
     product = models.ForeignKey(ProductForPrediction, on_delete = models.CASCADE)
-    amount = models.IntegerField()
+    amount = models.IntegerField(MinValueValidator(1))
 
     class Meta:
         db_table = "prediction_tables_sale_prediction"
