316da33221a8286ea52142b9cd6f4782ffefa3e1
max-dmytryshyn <maksumus22@gmail.com>
2021-05-26 13:12:34 +0300

    Create serializer for SalePrediction model

--- Backend(Django)/farcast/prediction_tables/serializers.py
+++ Backend(Django)/farcast/prediction_tables/serializers.py
@@ -1,9 +1,15 @@
 from rest_framework import serializers
 
-from .models import ProductForPrediction
+from .models import ProductForPrediction, SalePrediction
 
 
 class ProductForPredictionSerializer(serializers.ModelSerializer):
     class Meta:
         model = ProductForPrediction
+        fields = '__all__'
+
+
+class SalePredictionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SalePrediction
         fields = '__all__'
