d96de1db79f820f5b8db5c5c01609403c47b1795
Mikola-K <hotoneshot.ua@gmail.com>
2021-05-27 17:33:11 +0300

    Fix button name

--- Frontend(React)/src/components/profile-page/Profile.js
+++ Frontend(React)/src/components/profile-page/Profile.js
@@ -14,28 +14,28 @@ class Profile extends React.Component {
                             <h5 class="text">First name</h5>
                             <h3 class="text_two">First_name</h3>
                             <div class = "change-button" >
-                            <a href="#" target="_self" class="btn btn-primary btn-lg active" role="button" aria-pressed="true">Change</a></div>
+                            <a href="#" target="_self" class="btn btn-primary-profile btn-lg active" role="button" aria-pressed="true">Change</a></div>
                         </div>
                         <hr/>
                         <div class="check_box">
                             <h5 class="text">Last name</h5>
                             <h3 class="text">Last_name</h3>
                             <div class = "change-button" >
-                            <a href="#" target="_self" class="btn btn-primary btn-lg active" role="button" aria-pressed="true">Change</a></div>
+                            <a href="#" target="_self" class="btn btn-primary-profile btn-lg active" role="button" aria-pressed="true">Change</a></div>
                         </div>
                         <hr/>
                         <div class="check_box">
                             <h5 class="text">Email</h5>
                             <h3 class="text">MyEmail@gmail.com</h3>
                             <div class = "change-button" >
-                            <a href="#" target="_self" class="btn btn-primary btn-lg active" role="button" aria-pressed="true">Change</a></div>
+                            <a href="#" target="_self" class="btn btn-primary-profile btn-lg active" role="button" aria-pressed="true">Change</a></div>
                         </div>
                         <hr/>
                         <div class="check_box">
                             <h5 class="text">Phone number</h5>
                             <h3 class="text">+380869297399</h3>
                             <div class = "change-button" >
-                            <a href="#" target="_self" class="btn btn-primary btn-lg active" role="button" aria-pressed="true">Change</a></div>
+                            <a href="#" target="_self" class="btn btn-primary-profile btn-lg active" role="button" aria-pressed="true">Change</a></div>
                         </div>
                         <hr/>
                         <div class="check_box">
@@ -47,7 +47,7 @@ class Profile extends React.Component {
                             <h5 class="text">Password</h5>
                             <h3 class="text">**********</h3>
                             <div class = "change-button" >
-                            <a href="#" target="_self" class="btn btn-primary btn-lg active" role="button" aria-pressed="true">Change</a></div>
+                            <a href="#" target="_self" class="btn btn-primary-profile btn-lg active" role="button" aria-pressed="true">Change</a></div>
                         </div>
                     </form>
                 </div>
