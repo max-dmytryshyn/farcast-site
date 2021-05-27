6ff644a05e35e6d24e5658401d68204a23219107
max-dmytryshyn <maksumus22@gmail.com>
2021-05-27 15:10:12 +0300

    Name product serializer properly

--- Backend(Django)/farcast/products/serializers.py
+++ Backend(Django)/farcast/products/serializers.py
@@ -3,7 +3,7 @@ from rest_framework import serializers
 from .models import Product
 
 
-class ProductForPredictionSerializer(serializers.ModelSerializer):
+class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = '__all__'
