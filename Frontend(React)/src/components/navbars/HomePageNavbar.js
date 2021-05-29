cf0e191255350467000362659e24ddb4c10941af
Mikola-K <hotoneshot.ua@gmail.com>
2021-05-29 22:02:23 +0300

    Make navbar depended on if user is logged in or not

--- Frontend(React)/src/components/navbars/HomePageNavbar.js
+++ Frontend(React)/src/components/navbars/HomePageNavbar.js
@@ -1,27 +1,37 @@
 import React from "react"
-
 import logo from "../../images/homepage_images/farcast_logo.svg"
 import "./HomePageNavbar.css"
 
-function Nav() {
-    return (
-        <div id="navbar">
-          <nav>
-            <img src = {logo} className="logo"></img>
-            <ul className="navlist"> 
-              <li><a href="#start">Home</a></li>
-              <li><a href="#features_part">About</a></li>
-              <li><a href="#pay_plans_part">Prices</a></li>
-              <li><a href="/login">Sign in</a></li>
-            </ul>
-            <div class="burger">
-                <div class="line1"></div>
-                <div class="line2"></div>
-                <div class="line3"></div>
-            </div>
-          </nav>
-        </div>
+
+function Nav () {
+  return (
+      <div id="navbar">
+          <script src="script.js"></script>
+       {localStorage.getItem("isLoggedIn") == "true"
+        ? 
+        <nav>
+        <img src = {logo} className="logo"></img>
+
+        <ul className="navlist"> 
+         <h3> Welcome, {localStorage.getItem("first name")}</h3>
+          <li><a href="/predictions">Predictions</a></li>
+          <li><a href="#">Statistics</a></li>
+          <li><a href="/profile">Profile</a></li>
+          <li><a href="/login">Sign out</a></li>
+        </ul>
+      </nav>
+        :
+        <nav>
+          <img src = {logo} className="logo"></img>
+          <ul className="navlist"> 
+            <li><a href="#start">Home</a></li>
+            <li><a href="#features_part">About</a></li>
+            <li><a href="#pay_plans_part">Prices</a></li>
+            <li><a href="/login">Sign in</a></li>
+          </ul>
+        </nav>
+      }
+      </div>
     )
 }
-
 export default Nav
