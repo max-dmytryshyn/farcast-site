1deed32765193f73a197e522b0d44b7cea828d60
Mikola-K <hotoneshot.ua@gmail.com>
2021-05-30 16:53:20 +0300

    Add burger-menu to navbar and style

--- Frontend(React)/src/components/navbars/HomePageNavbar.js
+++ Frontend(React)/src/components/navbars/HomePageNavbar.js
@@ -1,7 +1,65 @@
 import React from "react"
 import logo from "../../images/homepage_images/farcast_logo.svg"
+import { stack as Menu } from 'react-burger-menu'
 import "./HomePageNavbar.css"
 
+
+class Nav extends React.Component {
+  handleLoginOut = () => {
+    localStorage.removeItem("email");
+    localStorage.removeItem("username");
+    localStorage.removeItem("first name");
+    localStorage.removeItem("last name");
+    localStorage.removeItem("password");
+    localStorage.setItem("isLoggedIn", false);
+  }
+  showSettings = (event) => {
+    event.preventDefault();
+  }
+
+  render () { 
+    return (
+      <div >
+        {localStorage.getItem("isLoggedIn") == "true"
+          ?   
+          <div> 
+            <Menu right>
+              <a id="predictions" href="/predictions" className="menu-item">Predictions</a>
+              <a id="statistics" href="#" className="menu-item">Statistics</a>
+              <a id="profile" href="/profile" className="menu-item">Profile</a>
+              <a id="sing-out" href="/home" className="menu-item" onClick={this.handleLoginOut}>Sign out</a>
+            </Menu>
+            <div id="navbar"> 
+              <nav>
+                <img src = {logo} className="logo"></img>
+                <h3 id="welcome"> Welcome, {localStorage.getItem("first name")}</h3>
+              </nav>
+            </div>
+          </div>
+          :
+          <div id="navbar"> 
+            <nav>
+              <img src = {logo} className="logo"></img>
+              <ul className="navlist"> 
+                <li><a href="#start">Home</a></li>
+                <li><a href="#features_part">About</a></li>
+                <li><a href="#pay_plans_part">Prices</a></li>
+                <li><a href="/login">Sign in</a></li>
+              </ul>
+            </nav>
+          </div>
+        }
+      </div>
+    )
+  }
+}
+export default Nav
+
+
+
+
+
+{/*
 
 function Nav () {
   function handleLoginOut() {
@@ -15,7 +73,6 @@ function Nav () {
   }
   return (
       <div id="navbar">
-          <script src="script.js"></script>
        {localStorage.getItem("isLoggedIn") == "true"
         ? 
         <nav>        
@@ -43,3 +100,4 @@ function Nav () {
     )
 }
 export default Nav
+ */}
