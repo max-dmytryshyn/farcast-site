f2e0f8b2c5093107d4ac787bb8e949b71534ed1a
Mikola-K <hotoneshot.ua@gmail.com>
2021-05-30 19:32:07 +0300

    Fix navbar padding

--- Frontend(React)/src/components/prediction-table-page/PredictionTable.js
+++ Frontend(React)/src/components/prediction-table-page/PredictionTable.js
@@ -3,16 +3,15 @@ import DataList from "./DataList"
 import HomePageNavbar from "../navbars/HomePageNavbar"
 import "./PredictionTable.css"
 
-
 class PredictionTable extends Component {
     render () {
-        return <div>
+        return <div id = "testone">
+             <HomePageNavbar />
                 <div className="head">
                     <span className="bake"> Що пекти </span> 
                     <span className="search"> Пошук <hr className="division_line"/> </span>
                 </div>                    
                     <DataList />
-                    <HomePageNavbar />
                 </div>
     }
 }
