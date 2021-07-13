b660cec96c4571823558410a91ec680f3e088c4e
Mikola-K <hotoneshot.ua@gmail.com>
2021-07-13 14:15:48 +0300

    Add mobile phone and address fields. Change axios.

--- Frontend(React)/src/components/registration-page/Registration.js
+++ Frontend(React)/src/components/registration-page/Registration.js
@@ -12,6 +12,8 @@ class Registration extends React.Component {
             first_name: "", 
             last_name: "",
             email: "",
+            address: "",
+            mobile_phone: "",
         }
     }
     setUsername = (e) => {
@@ -29,20 +31,30 @@ class Registration extends React.Component {
     setEmail = (e) => {
         this.setState({ email: e.target.value });
     }
+    setAddress = (e) => {
+        this.setState({ address: e.target.value});
+    }
+    setMobilePhone = (e) => {
+        this.setState({ mobile_phone: e.target.value });
+    }
     handleRegister = () => {
-        const { username, password, first_name, last_name, email } = this.state;
+        const { username, password, first_name, last_name, email, address, mobile_phone } = this.state;
         axios.post('http://127.0.0.1:8000/users/all/', {
             password: password,
             username: username, 
             first_name: first_name, 
             last_name: last_name, 
-            email: email
+            email: email,
+            address: address,
+            mobile_phone: mobile_phone
         }).then(() => {
             localStorage.setItem ("email", email);
             localStorage.setItem ("username", username);
             localStorage.setItem ("first name", first_name);
             localStorage.setItem ("last name", last_name);
             localStorage.setItem ("password", password);
+            localStorage.setItem ("address", address);
+            localStorage.setItem ("mobile_phone", mobile_phone);
             localStorage.setItem("isLoggedIn", true)
             this.props.history.push('/profile')
         });
@@ -90,6 +102,19 @@ class Registration extends React.Component {
                         <div class="form__input-group">
                             <h5 class="email">Email</h5>
                             <input type="email" class="form__input" autofocus placeholder="Enter email"  onChange={this.setEmail}/>
+                            <div class="form__input-error-message"></div>
+                        </div>
+                        </form>
+                        <form class="form_three" id="formThree">
+                        <div class="form__message form__message--error"></div>
+                        <div class="form__input-group">
+                            <h5 class="address">Address</h5>
+                            <input type="text" class="form__input" autofocus placeholder="Enter your address"  onChange={this.setAddress}/>
+                            <div class="form__input-error-message"></div>
+                        </div>
+                        <div class="form__input-group">
+                            <h5 class="mobile__phone">Mobile phone</h5>
+                            <input type="tel" class="form__input" autofocus placeholder="Enter mobile phone"  onChange={this.setMobilePhone}/>
                             <div class="form__input-error-message"></div>
                         </div>
                         </form>
