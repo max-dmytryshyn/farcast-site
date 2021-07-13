798f9661d0318fb8b22fa6dc21180f8bfb3bf8c4
Mikola-K <hotoneshot.ua@gmail.com>
2021-07-13 14:14:55 +0300

    Add mobile phone and address fields

--- Frontend(React)/src/components/profile-page/Profile.js
+++ Frontend(React)/src/components/profile-page/Profile.js
@@ -9,7 +9,6 @@ class Profile extends React.Component {
         return (
             <div><ProfileNavbar/>
             <div className="Settings">
-                
                 <div class="container-profile">
                     <form class="form" id="settings">
                         <h1 class="form__title">Personal Account</h1>
@@ -30,6 +29,20 @@ class Profile extends React.Component {
                         <div class="check_box">
                             <h5 class="text">Email</h5>
                             <h3 class="email-text">{localStorage.getItem ("email")}</h3>
+                            <div class = "change-button" >
+                            <a href="#" target="_self" class="btn btn-primary-profile btn-lg active" role="button" aria-pressed="true">Change</a></div>
+                        </div>
+                        <hr/>
+                        <div class="check_box">
+                            <h5 class="text">Address</h5>
+                            <h3 class="address-text">{localStorage.getItem ("address")}</h3> 
+                            <div class = "change-button" >
+                            <a href="#" target="_self" class="btn btn-primary-profile btn-lg active" role="button" aria-pressed="true">Change</a></div>
+                        </div>
+                        <hr/>
+                        <div class="check_box">
+                            <h5 class="text">Mobile phone</h5>
+                            <h3 class="mobile-phone-text">{localStorage.getItem ("mobile_phone")}</h3>
                             <div class = "change-button" >
                             <a href="#" target="_self" class="btn btn-primary-profile btn-lg active" role="button" aria-pressed="true">Change</a></div>
                         </div>
