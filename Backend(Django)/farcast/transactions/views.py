aea663323a348ab4e9b1aab94adebc8aef48f0ec
max-dmytryshyn <maksumus22@gmail.com>
2021-08-10 18:26:38 +0300

    Create viewset for transaction and connect it with project with urls

--- Backend(Django)/farcast/transactions/views.py
+++ Backend(Django)/farcast/transactions/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import Transaction
+from .serializers import TransactionSerializer
+
+
+class TransactionViewSet(viewsets.ModelViewSet):
+    queryset = Transaction.objects.all()
+    serializer_class = TransactionSerializer
