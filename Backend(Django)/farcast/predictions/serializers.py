58549e44b48179f23f172b3dded84b59caf34c6a
max-dmytryshyn <maksumus22@gmail.com>
2021-08-10 18:25:47 +0300

    Fix prediction serializer post request

--- Backend(Django)/farcast/predictions/serializers.py
+++ Backend(Django)/farcast/predictions/serializers.py
@@ -1,11 +1,14 @@
 from rest_framework import serializers
 
 from .models import SalePrediction
+from products.serializers import ProductSerializer
 
 
 class SalePredictionSerializer(serializers.ModelSerializer):
     class Meta:
         model = SalePrediction
         fields = '__all__'
-        depth = 1
 
+    def to_representation(self, instance):
+        self.fields['product'] = ProductSerializer(read_only=True)
+        return super(SalePredictionSerializer, self).to_representation(instance)
