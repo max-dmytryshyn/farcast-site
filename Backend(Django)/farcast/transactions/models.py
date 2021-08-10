d9eb3ce80a0c05e9d48e5a9a03fbff57feb1f8c6
max-dmytryshyn <maksumus22@gmail.com>
2021-08-10 15:29:53 +0300

    Create transaction model

--- Backend(Django)/farcast/transactions/models.py
+++ Backend(Django)/farcast/transactions/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
-# Create your models here.
+from products.models import Product
+
+
+class Transaction(models.Model):
+    date = models.DateField()
+    time = models.TimeField()
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    amount = models.IntegerField(MinValueValidator(1))
+
+    class Meta:
+        db_table = "transactions"
