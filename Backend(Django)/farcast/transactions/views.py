23dcf8018814b4fc395b5d78ef23b52849fc3900
max-dmytryshyn <maksumus22@gmail.com>
2021-08-10 19:09:35 +0300

    Add support for transactions multiobjects requests

--- Backend(Django)/farcast/transactions/views.py
+++ Backend(Django)/farcast/transactions/views.py
@@ -1,9 +1,21 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 
 from .models import Transaction
 from .serializers import TransactionSerializer
+from rest_framework.response import Response
 
 
 class TransactionViewSet(viewsets.ModelViewSet):
     queryset = Transaction.objects.all()
     serializer_class = TransactionSerializer
+
+    def create(self, request, *args, **kwargs):
+        is_many = isinstance(request.data, list)
+        if not is_many:
+            return super(TransactionViewSet, self).create(request, *args, **kwargs)
+        else:
+            serializer = self.get_serializer(data=request.data, many=True)
+            serializer.is_valid(raise_exception=True)
+            self.perform_create(serializer)
+            headers = self.get_success_headers(serializer.data)
+            return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
