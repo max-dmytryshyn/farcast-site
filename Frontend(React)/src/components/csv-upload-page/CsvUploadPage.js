7a29a771db4432da833273752c7b07516db7f1be
Kleefeelf <wladfeel@gmail.com>
2021-08-12 14:12:21 +0300

    Update csv post

--- Frontend(React)/src/components/csv-upload-page/CsvUploadPage.js
+++ Frontend(React)/src/components/csv-upload-page/CsvUploadPage.js
@@ -2,40 +2,44 @@ import React from 'react'
 import HomePageNavbar from "../navbars/HomePageNavbar"
 import "./UploadPage.css"
 import axios from 'axios'
-import Papa from 'papaparse';
-
+import Papa from 'papaparse'
 
 
 class CsvUploadPage extends React.Component{
-
-    state = {
-        file: null
+    constructor() {
+        super()
+        this.state = {
+            file: undefined
+        }
+        this.updateData = this.updateData.bind(this)
     }
+    
 
-    handleFile(e) {
-        let file = e.target.files[0]       
-        var result = Papa.parse(file, {
-            download: true,
-            header: true,
-            complete: (result) => {
-                console.log(result)
-            }
-        })
-        this.setState({file: result})
+    importCSV = () => {
+        const { file } = this.state;
+        Papa.parse(file, {
+          complete: this.updateData,
+          header: true
+        });
+        
+    };
+    
+    updateData(result) {
+        var data = result.data;
+        axios.post('http://127.0.0.1:8000/transactions/all/', data)
+        console.log(data);
+      }
 
-
+    handleChange = event => {
+        this.setState({file: event.target.files[0]})
     }
 
     handleUpload() {
-        let file = this.state.file;
-        let formdata = new FormData();
-        formdata.append('csv_file', file)
-        axios({
-            method: 'post',
-            url: 'http://127.0.0.1:8000/transactions/all/',
-            data: formdata
-            
-        }).then()
+        
+        //console.log(this.state.file)
+
+        //axios.post('http://127.0.0.1:8000/transactions/all/', files)
+        
     }
     
     render () {
@@ -48,8 +52,8 @@ class CsvUploadPage extends React.Component{
                     <div id="deco_line_two"/>
                     <h1 className="import_txt">Іморпутуйте свої таблиці сюди</h1>
                     <label htmlFor="upload_btn">Вибрати ссв файл </label>
-                    <input type="file" name="file" className="hide" id="upload_btn" accept=".csv" onChange={(e)=>this.handleFile(e)} />
-                    <button type="button" onClick={(e)=>this.handleUpload(e)}>Загрузити</button>
+                    <input type="file" name="file" className="hide" id="upload_btn" accept=".csv" onChange={this.handleChange} />
+                    <button type="button" onClick={this.importCSV}>Загрузити</button>
                  </div>            
             </div>
         )
